f24000459a2ed463061caa2435ed5dfeb9ac62b0
mostafahassan097 <mostafahassan097@gmail.com>
2022-01-02 02:17:08 +0200

    insert table

--- createTB.sh
+++ createTB.sh
@@ -30,7 +30,7 @@ then
                         if [[ $pk == "Y" || $pk == "y" ]]
                         then
                             flag=1;
-                            echo -n ":#PK:" >> $Path/$DBname/$TBname;
+                            echo -n "#PK:" >> $Path/$DBname/$TBname;
                         else
                             break;
                         fi
@@ -39,9 +39,9 @@ then
                        read dataType;
                             case $dataType in 
                                 "int") 
-                                echo -n $colname":$dataType: " >> $Path/$DBname/$TBname ;;
+                                echo -n $colname":$dataType|" >> $Path/$DBname/$TBname ;;
                                 "string")  
-                                echo -n $colname":$dataType: " >> $Path/$DBname/$TBname ;;
+                                echo -n $colname":$dataType|" >> $Path/$DBname/$TBname ;;
                                 *) 
                                 echo "Not an Option !!";
                                 ((i = $i - 1));;
@@ -60,5 +60,5 @@ then
 else
     echo "Sorry Table Name Must Be Only Alphabet!!";
     sleep 2;
-    . ./createDB.sh;
+    . ./connectDB.sh;
 fi;
